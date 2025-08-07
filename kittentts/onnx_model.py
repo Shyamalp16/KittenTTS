cb606f0d42000d4c783318d3084dbb255025d31a
Shyamal Patel <61601225+Shyamalp16@users.noreply.github.com>
2025-08-06 20:53:02 -0400

    Update onnx_model.py

--- kittentts/onnx_model.py
+++ kittentts/onnx_model.py
@@ -3,6 +3,11 @@ import numpy as np
 import phonemizer
 import soundfile as sf
 import onnxruntime as ort
+import espeakng_loader
+from phonemizer.backend.espeak.wrapper import EspeakWrapper
+
+EspeakWrapper.set_library(espeakng_loader.get_library_path())
+EspeakWrapper.set_data_path(espeakng_loader.get_data_path())
 
 
 def basic_english_tokenize(text):
